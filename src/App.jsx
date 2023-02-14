c0fc839816d8d202ca2cdc02238285cd6211acfc
Thom Sterenborg <thomsterenborg@gmail.com>
2023-02-14 20:34:53 +0100

    Finhes Props Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { DrinkButtons } from "./components/DrinkButtons";
+import { DrinkChoice } from "./components/DrinkChoice";
+import { coffee, tea } from "./utils/data";
 
 export const App = () => {
   const greeting = "Hello, welcome to Winc Café";
+  const userDrink = coffee;
 
   return (
     <div className="app">
       <h1>{greeting}</h1>
-      <DrinkButtons />
+      <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+      <DrinkChoice drink={userDrink} />
     </div>
   );
 };
