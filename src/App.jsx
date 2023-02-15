cef3ed0825a4d50bc27c2556edb301ac142e5ba5
Thom Sterenborg <thomsterenborg@gmail.com>
2023-02-15 19:36:16 +0100

    Finished State Exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { DrinkButtons } from "./components/DrinkButtons";
 import { DrinkChoice } from "./components/DrinkChoice";
 import { coffee, tea } from "./utils/data";
 import { useState } from "react";
+import { DrinkSearch } from "./components/DrinkSearch";
 
 export const App = () => {
   const greeting = "Hello, welcome to Winc Café";
-  const [userDrink, setUserDrink] = useState(coffee);
+  const [userDrink, setUserDrink] = useState();
 
   return (
     <div className="app">
@@ -16,6 +17,7 @@ export const App = () => {
         <>
           <h1>{greeting}</h1>
           <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+          <DrinkSearch />
         </>
       )}
     </div>
